f82a041e34ffb383380d063d889dc6c810ab937f
imubarak234 <imubarak2424@gmail.com>
2023-08-22 16:56:00 +0100

    vm module

---
+++ misc/vm.js
@@ -0,0 +1,24 @@
+/**
+ * Example VM
+ * Running some arbitary commands
+ */
+
+// Dependecies
+const vm = require('vm');
+
+
+// Define a context for the script to run in
+let context = {
+    'foo': 25
+};
+
+// Define the script
+let script = new vm.Script(`
+    foo = foo * 2;
+    let bar = foo + 1;
+    let fizz = 52;
+`);
+
+// Run the script
+script.runInNewContext(context);
+console.log(context);
